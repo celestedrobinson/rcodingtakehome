313733a73134f002d481424494da8e2ed5003a64
Celeste Robinson <b.celeste.robinson@gmail.com>
2022-02-18 16:04:12 -0700

    fix compile error and run go fmt

--- main.go
+++ main.go
@@ -1,32 +1,30 @@
 package main
 
 import (
-	"log"
-    "net/http"
 	"encoding/json"
+	"log"
+	"net/http"
 
 	// "github.com/lithammer/shortuuid"
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 type Foo struct {
-	Name string	`json:"name"`
+	Name string `json:"name"`
 	Id   string `json:"id"`
 }
 
 var idMap = map[string]Foo{}
-
-
 
 func getFoo(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	// now get the id
 	vars := mux.Vars(r)
-    key := vars["id"]
+	key := vars["id"]
 	foo, exists := idMap[key]
 	if exists {
-		b, err:= json.Marshal(idMap[key])
+		b, err := json.Marshal(foo)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 		} else {
@@ -34,14 +32,15 @@ func getFoo(w http.ResponseWriter, r *http.Request) {
 			w.Write(b)
 		}
 	} else {
-		w.WriteHeader(http.)
+		w.WriteHeader(http.StatusNotFound)
 	}
 }
 
 func postFoo(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusOK)  
-    w.Write([]byte(`{"message": "post called"}`))
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "post called"}`))
 }
 
 func deleteFoo(w http.ResponseWriter, r *http.Request) {
@@ -50,19 +49,19 @@ func deleteFoo(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["id"]
 	delete(idMap, key)
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte(`{"message": "delete called"}`))
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "delete called"}`))
 }
 
 func main() {
 	r := mux.NewRouter().StrictSlash(true)
-	
+
 	idMap["a"] = Foo{
-		Id: "a",
+		Id:   "a",
 		Name: "Al",
 	}
 	r.HandleFunc("/foo/{id}", getFoo).Methods(http.MethodGet)
 	r.HandleFunc("/foo", postFoo).Methods(http.MethodPost)
 	r.HandleFunc("/foo/{id}", deleteFoo).Methods(http.MethodDelete)
-    log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
